81b99e5e423a690f6a154b5c1d7e9546d50db640
TheChyz <thechyz@hotmail.com>
2014-11-05 12:58:15 -0800

    Create lexemesTypes.cpp

---
+++ lexemesTypes.cpp
@@ -0,0 +1,43 @@
+/*
+ * lexemesTypes.cpp
+ *
+ * Created on: Oct 27, 2014
+ *
+ */
+
+#include "lexemesTypes.h"
+
+namespace ensc251{
+
+  //***Complete the implementation of the following functions: ***
+
+  //This function returns TRUE if the input is identifier else FALSE
+  bool isIdentifier(const string &lexeme) { }
+
+  //This function returns TRUE if the input is one of the operators defined in the variable
+  //"tableOfOperators" in "lexemesTypes.h" else it returns FALSE
+  bool isOperator(const string &lexeme) { }
+
+  //This function returns TRUE if the input is one of the punctuators defined in the variable
+  //"tableOfPunctuators" in "lexemesTypes.h" else it returns FALSE
+  bool isPunctuator(char lexeme) { }
+
+  //This function returns TRUE if the input is one of the keywords defined in the variable
+  //"tableOfKeywords" in "lexemesTypes.h" else it returns FALSE
+  bool isKeyword(const string &lexeme) { }
+
+  //This function returns TRUE if the input is one of the boolean defined in the variable
+  //"booleanValues" in "lexemesTypes.h" else it returns FALSE
+  bool isBooleanValue(const string &lexeme) { }
+
+  //This function returns TRUE if the input is integer literal else FALSE
+  bool isIntegerLiteral(const string &lexeme) { }
+
+  //This function returns TRUE if the input is float literal else FALSE
+  bool isFloatLiteral(const string &lexeme) { }
+
+  //This function returns TRUE if the input is string literal else FALSE
+  bool isStringLiteral(const string &lexeme) { }
+}
+
+
