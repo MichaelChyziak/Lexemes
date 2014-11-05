486fca74c088604d2c7f33f42b83e6131596ef47
TheChyz <thechyz@hotmail.com>
2014-11-05 12:59:14 -0800

    Create parserClasses.cpp

---
+++ parserClasses.cpp
@@ -0,0 +1,50 @@
+//Use only the following libraries:
+#include "parserClasses.h"
+#include <string>
+
+//Complete the implementation of the following member functions:
+
+//****TokenList class function definitions******
+
+//Creates a new token for the string input, str
+//Appends this new token to the TokenList
+//On return from the function, it will be the last token in the list
+void TokenList::append(const string &str) { }
+
+//Appends the token to the TokenList if not null
+//On return from the function, it will be the last token in the list
+void TokenList::append(Token *token) { }
+
+//Removes the token from the linked list if it is not null
+//Deletes the token
+//On return from function, head, tail and the prev and next Tokens (in relation to the provided token) may be modified.
+void TokenList::deleteToken(Token *token) { }
+
+//Input: a pointer to a token
+//Output: it won't return anything, but within function, it should set the token class (i.e. token->stringType)
+//Note: one can invoke this function before adding token to the token list
+void TokenList::setTokenClass(Token *token) { }
+
+
+//****Tokenizer class function definitions******
+
+//Computes a new tokenLength for the next token
+//Modifies: size_t tokenLength, and bool complete
+//(Optionally): may modify offset
+//Does NOT modify any other member variable of Tokenizer
+void Tokenizer::prepareNextToken(){ }
+
+//Sets the current string to be tokenized
+//Resets all Tokenizer state variables
+//Calls Tokenizer::prepareNextToken() as the last statement before returning.
+void Tokenizer::setString(string *str) { }
+
+//Returns the next token. Hint: consider the substr function
+//Updates the tokenizer state
+//Updates offset, resets tokenLength, updates processingABC member variables
+//Calls Tokenizer::prepareNextToken() as the last statement before returning.
+string Tokenizer::getNextToken() { }
+
+
+
+
