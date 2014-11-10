46bed51416a5e53b17a250e0cb49098a557ef35d
TheChyz <thechyz@hotmail.com>
2014-11-09 20:37:45 -0800

    Update lexemesTypes.cpp

--- lexemesTypes.cpp
+++ lexemesTypes.cpp
@@ -11,15 +11,16 @@ namespace ensc251{
 
 	//***Complete the implementation of the following functions: ***
 
+	bool isCommented = false;
+
 	//This function returns TRUE if the input is identifier else FALSE
 	bool isIdentifier(const string &lexeme) {
 		//http://msdn.microsoft.com/en-us/library/565w213d.aspx	
 		//create an array of possible identifier characters and natural numbers, as well as the arrays sizes
 		const char identifierChars[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
 										'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_' };
-		const char naturalNumbers[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 		int sizeofIdentifierChars = 53;
-		int sizeOfNaturalNumbers = 10;
+		int sizeOfNaturalNumbers = sizeof(tableOfNaturalNumbers) / sizeof(tableOfNaturalNumbers[0]);
 		
 		int offset = 0;
 
@@ -29,47 +30,17 @@ namespace ensc251{
 			return false;
 		}
 
-		//checks that our string starts with an allowed character (identifierChars), if it is then increase offset by 1
-		if (lexeme.length() > 0) {
-			for (int i = 0; i < sizeofIdentifierChars; i++) {
-				if (lexeme.at(0) == identifierChars[i]) {
-					offset++;
-					break;
-				}
-			}
-		}
-
-		//checks if our string is "__" or "_" + a capital letter since those are reserved for the for C++ implementations
-		//if it is, return false
-		if (lexeme.length() > 1) {
-			if (lexeme.at(0) == '_' && lexeme.at(1) == '_') {
-				return false;
-			}
-			if (lexeme.at(0) == '_') {
-				for (int i = 26; i < (sizeofIdentifierChars - 1); i++) {
-					if (lexeme.at(1) == identifierChars[i]) {
-						return false;
-					}
-				}
-			}
-		}
-		
-		//checks that the other characters past the first are all allowed characters (letters, numbers, and underscores)
-		//if they are then increase offset by 1
-		for (int i = 1; i < lexeme.length(); i++) {
+		//checks that our string is all filled with an allowed character (identifierChars), if it is then increase offset by 1
+		for (int i = 0; i < lexeme.length(); i++) {
 			for (int j = 0; j < sizeofIdentifierChars; j++) {
 				if (lexeme.at(i) == identifierChars[j]) {
 					offset++;
 					break;
 				}
 			}
-			for (int j = 0; j < sizeOfNaturalNumbers; j++) {
-				if (lexeme.at(i) == naturalNumbers[j]) {
-					offset++;
-					break;
-				}
-			}
 		}
+		
+		
 		//returns true if the offset equals the length of the string
 		if (offset > 0 && offset == lexeme.length()) {
 			return true;
@@ -86,10 +57,8 @@ namespace ensc251{
 		//compares the temporary string to all of the strings from "tableOfOperators" in "lexemesTypes.h"
 		//returns true if the string matches to one of the strings in the array, false otherwise
 		for (int i = 0; i < temp.length(); i++) {
-			for (int j = 0; j < sizeOfOperators; j++) {
-				if (temp == tableOfOperators[j]) {
-					return true;
-				}
+			if (temp == tableOfOperators[i]) {
+				return true;
 			}
 		}
 		return false;
@@ -121,10 +90,8 @@ namespace ensc251{
 		//compares the temporary string to all of the strings from "tableOfKeywords" in "lexemesTypes.h"
 		//returns true if the string matches to one of the strings in the array, false otherwise
 		for (int i = 0; i < temp.length(); i++) {
-			for (int j = 0; j < sizeOfKeywords; j++) {
-				if (temp == tableOfKeywords[j]) {
-					return true;
-				}
+			if (temp == tableOfKeywords[i]) {
+				return true;
 			}
 		}
 		return false;
@@ -151,34 +118,98 @@ namespace ensc251{
 	//This function returns TRUE if the input is integer literal else FALSE
 	bool isIntegerLiteral(const string &lexeme) {
 		string temp = lexeme; //creates a temporary string variable to hold the value of lexeme
-
 		//http://en.cppreference.com/w/cpp/language/integer_literal
-		bool isDecimal = false;
-		bool isOctal = false;
-		bool isHex = false;
-		bool isBinary = false;
-
-		const char naturalNumbers[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		bool isInt = false;
+		int offset = 0;
 		const char hexLetters[] = { 'x', 'X', 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F' };
 		const char integerSuffix[] = { 'u', 'U', 'l', 'L' };
-
-		int sizeOfNaturalNumbers = 10;
+		int sizeOfNaturalNumbers = sizeof(tableOfNaturalNumbers) / sizeof(tableOfNaturalNumbers[0]);
 		int sizeOfHexLetters = 14;
 		int sizeOfIntegerSuffix = 4;
 
+		//Checks every character of the string to see if it is an IntegerLiteral, if it is then increase offset by 1
+		for (int i = 0; i < temp.length(); i++) {
+			for (int j = 0; j < sizeOfNaturalNumbers; j++) {
+				if (temp.at(i) == tableOfNaturalNumbers[j]) {
+					offset++;
+				}
+			}
+			if (temp.at(0) == '0') {
+				for (int j = 0; j < sizeOfHexLetters; j++) {
+					if (temp.at(i) == hexLetters[j]) {
+						offset++;
+					}
+				}
+			}
+			for (int j = 0; j < sizeOfIntegerSuffix; j++) {
+				if (temp.at(i) == integerSuffix[j]) {
+					offset++;
+				}
+			}
+		}
+
+		//make sure that the string is not empty and that the amount of offset integersLiteral cases is equal to the length of the string
+		//if so return true, otherwise false
+		if (offset > 0 && offset == temp.length()) {
+			return true;
+		}
 		return false;
 	}
-
 	//This function returns TRUE if the input is float literal else FALSE
 	bool isFloatLiteral(const string &lexeme) {
 		string temp = lexeme; //creates a temporary string variable to hold the value of lexeme
+		//http://msdn.microsoft.com/en-us/library/tfh6f0w2.aspx
+		int offset = 0;
+		int sizeOfNaturalNumbers = sizeof(tableOfNaturalNumbers) / sizeof(tableOfNaturalNumbers[0]);
+		const char floatingPointCharacters[] = { '.', 'e', 'E', 'l', 'L', 'f', 'F', '+', '-' };
+		int floatingPointCharactersSize = 9;
+
+		//Checks every character of the string to see if it is a Float Literal, if it is then increase offset by 1
+		for (int i = 0; i < temp.length(); i++) {
+			for (int j = 0; j < sizeOfNaturalNumbers; j++) {
+				if (temp.at(i) == tableOfNaturalNumbers[j]) {
+					offset++;
+				}
+			}
+			for (int j = 0; j < floatingPointCharactersSize; j++) {
+				if (temp.at(i) == floatingPointCharacters[j]) {
+					offset++;
+				}
+			}
+		}
+		
+		//make sure that the string is not empty and that the amount of offset floatLiteral cases is equal to the length of the string
+		//if so return true, otherwise false
+		if (offset > 0 && offset == temp.length()) {
+			return true;
+		}
 		return false;
 	}
-
 	//This function returns TRUE if the input is string literal else FALSE
 	bool isStringLiteral(const string &lexeme) {
 		string temp = lexeme; //creates a temporary string variable to hold the value of lexeme
+		//http://msdn.microsoft.com/en-us/library/69ze775t.aspx <- this lists the different types of string literals
+		if (temp.length() > 1){
+			if (temp.at(0) == '"' && temp.at(1) == '"'){
+				return true;
+			}
+		}
+		// if the token is a narrow string variable the token length must be atleast 2 and have a quotation mark at the begining or end of the string
+
+
+		/*
+		if (temp.length() > 3){
+		if( (temp.at(0) == 'L' || temp.at(0) == 'R')&&  temp.at(1) == '"' && temp.at(temp.length()-1) == '"' ){
+		return true;
+		}}
+		// if the token is a wide non-raw literal  or a narrow raw literal you must have atleast  3 terms i.e 1 L or R, a quotation marks preceding this and ending quotation mark
+		if (temp.length() > 4){
+		if( (temp.at(0) == 'L' ) && (temp.at(1) == 'R' ) && temp.at(2) == '"' && temp.at(temp.length()-1) == '"' ){
+		return true;
+		}}
+		// if the token is a wide raw literal you must have atleast  4 terms i.e LR" and a quotation mark at the end of the string.
+		*/  // This covers a lexical case that only microsoft considers.
+
 		return false;
 	}
 }
-
